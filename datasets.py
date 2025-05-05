d4c00d47b623db72ead4e2cb775ee78495098417
Richard Ardelean <ardeleaneugenrichard@gmail.com>
2025-05-05 22:22:23 +0300

    ongoing prep

--- datasets.py
+++ datasets.py
@@ -4,7 +4,7 @@ from dataset_parsing import simulations_dataset as ds
 def load_all_data():
     datasets = []
     # for simulation_number in range(1,96):
-    for simulation_number in [38, 65, 80]:
+    for simulation_number in [65]:
         if simulation_number == 25 or simulation_number == 44 or simulation_number == 78:
             continue
         datasets.append((f"Sim{simulation_number}", ds.get_dataset_simulation(simNr=simulation_number)))
