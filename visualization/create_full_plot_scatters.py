eff18cf8c3ddb9b41335413d33c64fbb138ce936
Richard Ardelean <ardeleaneugenrichard@gmail.com>
2025-05-07 06:31:28 +0300

    typos fix

--- visualization/create_full_plot_scatters.py
+++ visualization/create_full_plot_scatters.py
@@ -207,7 +207,7 @@ def main_metrics():
             file_dir=fig_folder + scatter_plot_folder,
             output_path=fig_folder + f"{scatter_plot_folder}.png",
             file_format="png",  # or "svg"
-            figsize=(8, 10),
+            figsize=(6, 10),
             dpi=600,
             hspace=0.01,
             y=0.9,
